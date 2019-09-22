c5360ca4b7ce4e0ccaa07d444631a0cb6b4c9405
miguel angel bravo <miguelbravo153@gmail.com>
2019-09-22 13:17:15 -0500

    configurando elemento

--- Api-blog/app/helpers/jwtAuth.php
+++ Api-blog/app/helpers/jwtAuth.php
@@ -1,25 +1,28 @@
-<?php 
+<?php
 
 //creamos el namespace para acceder a cualquier lado mas rapido 
-namespace App\helpers; 
+namespace App\helpers;
 
 use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\DB;
-use App\User; 
+use App\User;
 
 
-class jwtAuth {
+class jwtAuth
+{
 
-    public $key; 
+    public $key;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->key = "esta_es_una_clave_super_secreta*125";
     }
 
 
 
     //funcion para loguear al usuario 
-    public function signUp($userName, $password, $getToken = null){
+    public function signUp($userName, $password, $getToken = null)
+    {
         /*buscar si existe el usuario y contraseña */
 
         //esto me retornara el primer resultado, podemos usar get. podemos usar all que nos trae todos los datos 
@@ -31,13 +34,13 @@ class jwtAuth {
         /*comprobar si existen */
         //inicializa en false. si encuentra algo, este lo retornara como objeto, si es correcto ingresa
         $signup = false;
-        if(is_object($user)){
+        if (is_object($user)) {
             $signup = true;
         }
 
         /*generar el token con los datos del usuario identificado */
         //en caso de ser true generaremos el token
-        if($signup){
+        if ($signup) {
 
             $token = array(
                 'sub'       => $user->id,
@@ -48,34 +51,34 @@ class jwtAuth {
                 'exp'       => time() + (7 * 24 * 60 * 60) // el token tendra una vida util de una semana 
             );
 
-            $jwt = JWT::encode($token,$this->key, 'HS256'); // token codificado 
+            $jwt = JWT::encode($token, $this->key, 'HS256'); // token codificado 
 
-            $jwtDecode = JWT::decode($jwt, $this->key,['HS256']); //token decodificado 
+            $jwtDecode = JWT::decode($jwt, $this->key, ['HS256']); //token decodificado 
 
 
             //devolver los datos decodificados o el token en funcion de un parametro 
 
-            if(is_null($getToken)){
+            if (is_null($getToken)) {
                 $data = $jwt;
-            }else{
-                $data =$jwtDecode;
+            } else {
+                $data = $jwtDecode;
             }
-
-        }else{
+        } else {
             $data = array(
-                'respuesta' => 'error',
+                'estado' => 'error',
+                'codigo' => 200,
                 'mensaje' => 'login incorrecto'
             );
         }
 
-        return $data;  
-
+        return $data;
     }
 
 
 
     //funcion para verificar el token.
-    public function checkToken($jwt, $getIdentity  = false){
+    public function checkToken($jwt, $getIdentity  = false)
+    {
         $auth = false; //por defecto la autenticacion del token sera falso 
 
         //este codigo es propenso a errores, por eso lo agregamos en un try 
@@ -83,22 +86,21 @@ class jwtAuth {
             //eliminados las comillas del token 
             str_replace('"', '', $jwt);
 
-           $tokenDeco = JWT::decode($jwt, $this->key, ['HS256']); 
+            $tokenDeco = JWT::decode($jwt, $this->key, ['HS256']);
         } catch (\UnexpectedValueException $th) {
             $auth = false;
-        }catch (\DomainException $th) {
+        } catch (\DomainException $th) {
             $auth = false;
         }
 
 
-    (!empty($tokenDeco) && is_object($tokenDeco) && isset($tokenDeco->sub))? $auth = true: $auth = false;
+        (!empty($tokenDeco) && is_object($tokenDeco) && isset($tokenDeco->sub)) ? $auth = true : $auth = false;
 
-    if($getIdentity){
-        return $tokenDeco;
-    }
+        if ($getIdentity) {
+            return $tokenDeco;
+        }
 
-    return $auth;
+        return $auth;
+    } //final funcion token 
 
-    }//final funcion token 
-   
 }//final clase
